d0cd4635514d5c396648b28657b5985a70881ab3
Chetan Parthiban <cparthiban08@gmail.com>
2020-12-10 14:45:36 -0600

    added datetime import

--- voxel_net.py
+++ voxel_net.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn 
 import torch.nn.functional as F
 import spconv
+from datetime import datetime
 
 def createSparseConvBlock(in_channels, out_channels, kernel_size, indice_key=None, stride=1, padding=1,
                    conv_type='subm'):
